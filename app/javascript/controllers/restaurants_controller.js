fa99d6a5be58f90db0efa9923688e21766f9f365
Andrew Mason <andrewmcodes@protonmail.com>
2019-10-17 21:15:56 -0400

    add console logs and remove room

--- app/javascript/controllers/restaurants_controller.js
+++ app/javascript/controllers/restaurants_controller.js
@@ -4,10 +4,16 @@ import { confetti } from 'dom-confetti'
 
 export default class extends Controller {
   connect () {
+    console.log("connected")
     StimulusReflex.register(this)
   }
 
+  beforeReflex () {
+    console.log('before reflex')
+  }
+
   afterReflex (anchorElement) {
+    console.log("after reflex")
     confetti(anchorElement)
   }
 }
