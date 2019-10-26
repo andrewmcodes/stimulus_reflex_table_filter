345212c35c69171d31200400848a70995c9ab3bb
pjf <pjforde1978@gmail.com>
2019-10-25 22:07:35 -0400

    outer window

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -4,7 +4,7 @@ Kaminari.configure do |config|
   config.default_per_page = 10
   # config.max_per_page = nil
   config.window = 2
-  # config.outer_window = 0
+  config.outer_window = 1
   # config.left = 0
   # config.right = 0
   # config.page_method_name = :page
